7f507e02d7e8f0fe487bf444eddd4a369d606a8d
Victor |Stalker| Skurikhin <stalker@quake.ru>
2015-07-28 09:36:07 +0300

    Changes to be committed: 	new file:   Algs4Prt18puzzle/.classpath 	new file:   Algs4Prt18puzzle/.project 	new file:   Algs4Prt18puzzle/build.xml 	new file:   Algs4Prt18puzzle/dist/README.TXT 	new file:   Algs4Prt18puzzle/manifest.mf 	new file:   Algs4Prt18puzzle/nbproject/build-impl.xml 	new file:   Algs4Prt18puzzle/nbproject/genfiles.properties 	new file:   Algs4Prt18puzzle/nbproject/private/config.properties 	new file:   Algs4Prt18puzzle/nbproject/private/private.properties 	new file:   Algs4Prt18puzzle/nbproject/private/private.xml 	new file:   Algs4Prt18puzzle/nbproject/project.properties 	new file:   Algs4Prt18puzzle/nbproject/project.xml 	new file:   Algs4Prt18puzzle/src/Algs4Prt18puzzle.java

---
+++ Algs4Prt18puzzle/src/Algs4Prt18puzzle.java
@@ -0,0 +1,30 @@
+/* Algs4Prt18puzzle.java */
+/* $Date$
+ * $Id$
+ * $Version: 0.1$
+ * $Revision: 1$
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author skurvikn
+ */
+public class Algs4Prt18puzzle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        StdOut.println("Hello world!");
+    }
+}
+
+/* vim: syntax=java:fileencoding=utf-8:fileformat=unix:tw=78:ts=4:sw=4:sts=4:et
+ */
+//EOF
