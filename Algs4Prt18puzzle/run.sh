f387eceba2acc70abcdd999612565ed247d2c530
Victor |Stalker| Skurikhin <stalker@quake.ru>
2015-08-19 16:18:47 +0300

    Changes to be committed: 	modified:   Algs4Prt18puzzle/run.sh

--- Algs4Prt18puzzle/run.sh
+++ Algs4Prt18puzzle/run.sh
@@ -5,7 +5,8 @@ if [ "x" != "x$1" ] ; then
 else
   for F in 8puzzle/*.txt ; do
      echo $F
-     java -jar dist/${JAR} $F|egrep -e 'Minimum number of moves|No solution possible'
+     java -Xms128m -Xmx256m -XX:PermSize=256M -jar dist/${JAR} $F|
+     egrep -e 'Minimum number of moves|No solution possible'
      sleep 1
   done
 fi
